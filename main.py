1d16f87b22b3309ea05184fc0395eb9e8cc941a5
maooam27 <mfede478@gmail.com>
2024-07-23 20:45:34 +0200

    adding graphics

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+import random
 import tkinter as tk
+
+bets = ["rock", "paper", "scissors"]
 
 
 class App(tk.Tk):
+    gameStarted = False
+
+    def play(self, bet: str):
+        print(bet)
+        self.choice.set(bet)
+
+    def rock(self):
+        self.play("rock")
+
+    def paper(self):
+        self.play("paper")
+
+    def scissors(self):
+        self.play("scissors")
+
     def __init__(self):
         super().__init__()
         self.title("Rock - Paper - Scissors")
@@ -9,11 +27,31 @@ class App(tk.Tk):
         self.resizable(False, False)
         self.configure(bg="#f0f0f0")
 
-        self.label = tk.Label(self, text="Rock - Paper - Scissors", font=("Arial", 26), fg="black", bg="#f0f0f0")
-        self.label.pack(pady=20)
+        self.label = tk.Label(self, text="Rock - Paper - Scissors", font=("Arial", 28), fg="black", bg="#f0f0f0")
+        self.label.pack(pady=30)
 
-        self.bot_choice = tk.Entry(self, bg="white", font=("Arial", 20))
+        self.choice = tk.StringVar()
+        self.choice.set("place your bet")
+
+        self.bot_choice = tk.Entry(self, bg="white", font=("Arial", 20), textvariable=self.choice, state="readonly",
+                                   justify="center")
         self.bot_choice.pack(pady=10)
+
+        # Frame for the buttons
+        self.stack = tk.Frame(self, bg="#f0f0f0")
+        self.stack.pack(pady=30)
+
+        self.rock = tk.Button(self.stack, text="Rock", font=("Arial", 20), bg="white", width=5,
+                              borderwidth=3, command=self.rock)
+        self.rock.pack(side="left", padx=10)
+
+        self.paper = tk.Button(self.stack, text="Paper", font=("Arial", 20), bg="white", width=5,
+                               borderwidth=3, command=self.paper)
+        self.paper.pack(side="left", padx=10)
+
+        self.scissors = tk.Button(self.stack, text="Scissors", font=("Arial", 20), bg="white", width=5,
+                                  borderwidth=3, command=self.scissors)
+        self.scissors.pack(side="left", padx=10)
 
 
 if __name__ == "__main__":
